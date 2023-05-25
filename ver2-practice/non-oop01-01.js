8a6b2ffcef6b77fc2e8d5878da72a394c3cbd539
planethouki <26595148+planethouki@users.noreply.github.com>
2023-05-25 17:58:12 +0900

    ver2 practice

---
+++ ver2-practice/non-oop01-01.js
@@ -0,0 +1,26 @@
+// id="text" のエレメントを取得
+const textElement = document.getElementById("text");
+
+// エレメントの内部テキストを更新するには？
+
+
+// エレメントの内部テキストを削除するには？
+
+
+
+
+// id="button-container" のエレメントを取得
+const buttonContainerElement = document.getElementById("button-container");
+
+// エレメントの内部HTMLを削除するには？
+
+
+
+
+// ボタンエレメントを作成するには？
+const button = 
+
+// ボタンの文字をセット
+button.innerText = "Click Me";
+
+// buttonContainerElement エレメントの配下にセットするには？
---
+++ ver2-practice/non-oop01-02.js
@@ -0,0 +1,15 @@
+const buttonContainerElement = document.getElementById("button-container");
+
+// ボタンエレメントを作成
+const button = document.createElement("button");
+
+// ボタンの文字をセット
+button.innerText = "Click Me";
+
+// ボタンがクリックされた時に、"Clicked"と表示してください。
+button.
+
+
+
+// 他のエレメントにセットする
+buttonContainerElement.appendChild(button);
