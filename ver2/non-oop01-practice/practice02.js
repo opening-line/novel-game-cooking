c4b0d3dc1d70bf2bda22cdf301283220450b3489
planethouki <26595148+planethouki@users.noreply.github.com>
2023-06-06 18:13:21 +0900

    update

--- ver2/non-oop01-practice/practice02.js
+++ ver2/non-oop01-practice/practice02.js
@@ -1,15 +1,14 @@
-const boxElement = document.getElementById("box");
-
-// ボタンエレメントを作成
-const button = document.createElement("button");
-
-// ボタンの文字をセット
-button.innerText = "Click Me";
-
-// ボタンがクリックされた時に、"Clicked"と表示してください。
+const textElement = document.getElementById("text");
 
 
+// async 関数で囲んでください
+for (let i = 0; i < 100; i++) {
 
+    // 何文字か追記してください
+    
 
-// 他のエレメントにセットする
-boxElement.appendChild(button);
+    // 256ミリ秒待機するコードを書いてください。
+    
+    
+}
+
--- ver2/non-oop01-practice/practice03.js
+++ ver2/non-oop01-practice/practice03.js
@@ -1,14 +1,15 @@
-const textElement = document.getElementById("text");
+const boxElement = document.getElementById("box");
+
+// ボタンエレメントを作成
+const button = document.createElement("button");
+
+// ボタンの文字をセット
+button.innerText = "Click Me";
+
+// ボタンがクリックされた時に、"Clicked"と表示してください。
 
 
-// async 関数で囲んでください
-for (let i = 0; i < 100; i++) {
 
-    // 何文字か追記してください
-    
 
-    // 256ミリ秒待機するコードを書いてください。
-    
-    
-}
-
+// 他のエレメントにセットする
+boxElement.appendChild(button);
